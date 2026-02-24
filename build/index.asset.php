93db475768174dc93e752282325fccfc17f882ff
michakrapp <michakrapp@gmail.com>
2026-02-24 23:35:14 +0100

    chore: build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c6918ebb1e9c365fe436');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '844ebbb538d549b45061');
